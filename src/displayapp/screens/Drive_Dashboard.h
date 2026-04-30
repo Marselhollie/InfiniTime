2dd0343cf9065ffbafbd530677027e8815cfde6e
Marselhollie <44211102+Marselhollie@users.noreply.github.com>
2026-04-30 13:28:31 -0700

    Create Drive_Dashboard.h

---
+++ src/displayapp/screens/Drive_Dashboard.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <lvgl/src/lv_core/lv_obj.h>
+#include "displayapp/screens/Screen.h"
+#include "displayapp/Apps.h"
+#include "displayapp/DisplayApp.h"
+
+namespace Pinetime {
+  namespace Applications {
+    namespace Screens {
+
+      class Drive_Dashboard : public Screen {
+      public:
+        explicit Drive_Dashboard(DisplayApp* app);
+        ~Drive_Dashboard() override;
+        bool OnTouchEvent(TouchEvents event) override;
+
+      private:
+        lv_obj_t* label;
+      };
+    }
+
+    template <>
+    struct AppTraits<Apps::DriveDashboard> {
+      static constexpr Apps app = Apps::DriveDashboard;
+      static constexpr const char* icon = "D";
+
+      static Screens::Screen* Create(AppControllers& controllers) {
+        return new Screens::Drive_Dashboard(controllers.displayApp);
+      }
+    };
+  }
+}
