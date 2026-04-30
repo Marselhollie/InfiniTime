778b2360e7ab21f4a50a518419f533ca21e824b8
Marselhollie <44211102+Marselhollie@users.noreply.github.com>
2026-04-30 05:13:04 -0700

    Update WatchFaceTerminal.cpp

--- src/displayapp/screens/WatchFaceTerminal.cpp
+++ src/displayapp/screens/WatchFaceTerminal.cpp
@@ -81,7 +81,7 @@ WatchFaceTerminal::WatchFaceTerminal(Controllers::DateTime& dateTimeController,
   labelMantra = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_color(labelMantra, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
   lv_label_set_long_mode(labelMantra, LV_LABEL_LONG_SROLL_CIRC);
-  lv_obj_set_style_local_anim_time(labelMantra, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, 2000);
+  lv_label_set_anim_speed(labelMantra, 80);
   lv_obj_set_width(labelMantra, 240);
   lv_obj_align(labelMantra, nullptr, LV_ALIGN_IN_BOTTOM_LEFT, 0, 0);
 
